814cac93011c652fc3744dbc61a51ab73bb5d283
Nomynameisjames <adavaonimisi@gmail.com>
2025-04-21 00:15:28 +0100

    made nav  bar on home screen responsive

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -8,41 +8,45 @@ import { LuSun } from "react-icons/lu"
 
 const Navbar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <Container maxW={"1140px"} px={4}  rounded={"lg"} bg={useColorModeValue("gray.900", "gray.900")}>
-      <Flex
-        h={16}
-        alignItems={"center"}
-        justifyContent={"space-between"}
-        flexDir={{
-          base:"column",
-          sm:"row"
-        }}
-      >
-      <Text
-        fontSize={{ base: "22", sm: "28"}}
-        fontWeight={"bold"}
-        textTransform={"uppercase"}
-        textAlign={"center"}
-        bgGradient={"linear(to-r, cyan.400, blue.500)"}
-        bgClip={"text"}
-      >
-        <Link to={"/"}> DTS Technical Test </Link>
+    <Box bg={useColorModeValue("gray.900", "gray.900")} py={2}>
+      <Container maxW="1140px" px={4}>
+        <Flex
+          h={16}
+          align="center"
+          justify="space-between"
+          direction="row"
+        >
+          <Text
+            fontSize={{ base: "md", sm: "xl", md: "2xl" }}
+            fontWeight="bold"
+            textTransform="uppercase"
+            whiteSpace="nowrap"
+            bgGradient="linear(to-r, cyan.400, blue.500)"
+            bgClip="text"
+          >
+            <Link to="/">DTS Technical Test</Link>
+          </Text>
 
-      </Text>
-      <HStack spacing={2} alignItems={"center"}>
-        <Link to={"/create"}>
-        <Button>
-          <FaPlus fontSize={20}/>
-        </Button>
-        </Link>
-        <Button onClick={ toggleColorMode }>{colorMode === "light" ? <IoMoon /> : <LuSun size="20" />}
-        </Button>
+          <HStack spacing={{ base: 1, sm: 2 }} ml={{ base: 2, sm: 4 }}>
+            <Link to="/create">
+              <Button size={{ base: "xs", sm: "sm" }} px={{ base: 2, sm: 3 }}>
+                <FaPlus fontSize={14} />
+              </Button>
+            </Link>
+            <Button
+              onClick={toggleColorMode}
+              size={{ base: "xs", sm: "sm" }}
+              px={{ base: 2, sm: 3 }}
+            >
+              {colorMode === "light" ? <IoMoon size={16} /> : <LuSun size={16} />}
+            </Button>
+          </HStack>
+        </Flex>
+      </Container>
+    </Box>
+  );
+};
 
-      </HStack>
-      </Flex>
-    </Container>
-  )
-}
-
-export default Navbar
+export default Navbar;
